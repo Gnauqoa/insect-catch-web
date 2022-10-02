3a0dbf7f416af19f3d875fa08b8604e4c31162e9
Gnauqoa <quanglng2022@gmail.com>
2022-10-03 01:19:54 +0700

    complete layout device list

--- src/features/home/DeviceList/index.jsx
+++ src/features/home/DeviceList/index.jsx
@@ -1,28 +1,40 @@
 import React from "react";
 
+const DeviceInfo = ({ name, create, status, energy }) => {
+  return (
+    <div className="flex flex-row">
+      <div className="flex flex-row w-[300px]">
+        <img />
+        <div className="flex flex-col">
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+      <div className="flex flex-row gap-́"></div>
+    </div>
+  );
+};
+
+const InfoShow = () => {
+  return (
+    <div className="flex flex-row">
+      <p className="font-[400] text-[20px] text-[#94918A] w-[300px]">Name</p>
+      <div className="flex flex-row gap-12">
+        <p className="font-[400] text-[20px] text-[#94918A]">Create</p>
+        <p className="font-[400] text-[20px] text-[#94918A]">Status</p>
+        <p className="font-[400] text-[20px] text-[#94918A]">Energy</p>
+      </div>
+    </div>
+  );
+};
+
 const DeviceList = () => {
   return (
     <div className="flex flex-col gap-6 ">
       <p className="font-[700] text-[24px]">Your device</p>
-      <div className="flex flex-row">
-        <p className="font-[400] text-[20px] text-[#94918A] w-[300px]">Name</p>
-        <div className="flex flex-row gap-12">
-          <p className="font-[400] text-[20px] text-[#94918A]">Create</p>
-          <p className="font-[400] text-[20px] text-[#94918A]">Status</p>
-          <p className="font-[400] text-[20px] text-[#94918A]">Energy</p>
-        </div>
-      </div>
+      <InfoShow />
       <div className="flex flex-col gap-8">
-        <div className="flex flex-row">
-          <div className="flex flex-row w-[300px]">
-            <img />
-            <div className="flex flex-col">
-              <p></p>
-              <p></p>
-            </div>
-          </div>
-          <div className="flex flex-row gap-́"></div>
-        </div>
+        <DeviceInfo />
       </div>
     </div>
   );
