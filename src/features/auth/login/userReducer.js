ce4349e8bd18a39e6cd53a188365d165458ef506
Gnauqoa <quanglng2022@gmail.com>
2023-04-17 22:13:44 +0700

    add user reducer

---
+++ src/features/auth/login/userReducer.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import get from "lodash/fp/get";
+
+const currentUserSlice = createSlice({
+  name: "currentUser",
+  initialState: {},
+  reducers: {
+    store(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+const { actions, reducer } = currentUserSlice;
+
+export const isLoggedInSelector = (state) =>
+  !!get("currentUser.id", state);
+
+export const { store } = actions;
+
+export default reducer;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "features/auth/login/userReducer.js";
 
 export const store = configureStore({
   reducer: {
-   
+    currentUser: userReducer,
   },
 });
