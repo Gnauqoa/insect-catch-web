933e05d2ba3ef0a391bc8d50198b3b25b7ae66b7
Gnauqoa <quanglng2022@gmail.com>
2023-04-17 22:07:45 +0700

    add services/auth.js

---
+++ src/services/localStorage.js
@@ -0,0 +1,48 @@
+const REFRESH_TOKEN_NAME = "_lx.rt";
+const ACCESS_TOKEN_NAME = "_lx.at";
+const REMEMBER_ME_NAME = "_lx.rmm";
+
+const LONG_LIVE_TOKEN_TTL = 3600 * 24 * 7 * 1000;
+const SHORT_LIVE_TOKEN_TTL = 3600 * 1000;
+
+export const saveRefreshToken = (value) => {
+  const expiry =
+    new Date().getTime() +
+    (getRememberMe() ? LONG_LIVE_TOKEN_TTL : SHORT_LIVE_TOKEN_TTL);
+
+  const item = {
+    value: value,
+    expiry: expiry,
+  };
+  localStorage.setItem(REFRESH_TOKEN_NAME, JSON.stringify(item));
+};
+
+export const getRefreshToken = () => {
+  const itemStr = localStorage.getItem(REFRESH_TOKEN_NAME);
+  if (!itemStr) return null;
+  const item = JSON.parse(itemStr);
+  if (new Date().getTime() > item.expiry) {
+    clearTokens();
+    return null;
+  }
+  return item.value;
+};
+
+export const removeRefreshToken = () =>
+  localStorage.removeItem(REFRESH_TOKEN_NAME);
+export const saveAccessToken = (token) =>
+  token ? localStorage.setItem(ACCESS_TOKEN_NAME, token) : null;
+export const getAccessToken = () => localStorage.getItem(ACCESS_TOKEN_NAME);
+export const removeAccessToken = () =>
+  localStorage.removeItem(ACCESS_TOKEN_NAME);
+
+export const setRememberMe = (value) =>
+  localStorage.setItem(REMEMBER_ME_NAME, value);
+export const getRememberMe = () => !!localStorage.getItem(REMEMBER_ME_NAME);
+export const removeRememberMe = () => localStorage.removeItem(REMEMBER_ME_NAME);
+
+export const clearTokens = () => {
+  removeRefreshToken();
+  removeAccessToken();
+  removeRememberMe();
+};
